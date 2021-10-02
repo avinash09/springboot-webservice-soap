c10728e39af3fa72ed01fe6e8c3fca68ccba7751
unknown <avinashjbs@gmail.com>
2021-10-03 01:44:27 +0530

    initial commit

---
+++ src/main/java/com/example/howtodoinjava/SpringWebserviceSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.howtodoinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebserviceSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebserviceSoapApplication.class, args);
+	}
+
+}
